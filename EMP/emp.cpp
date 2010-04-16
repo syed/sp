ce06fd1db3632713e15a9d3f5426963858a8d569
syedam <syedam@whoseonefind-lm.(none)>
2010-04-17 00:42:42 +0530

    Emp sol ( easy  )

---
+++ EMP/emp.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+//stupid question 
+//only condition where the satement can be true and false at the same time is when 
+//m==n 
+int main()
+{
+	int n, m ;
+	cin>>n>>m;
+	if ( n == m )
+	{
+		cout<<n+m;
+	}
+	else
+	{
+		cout<<'0';
+	}
+}
