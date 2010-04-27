c44ae0fc93f4f8cff3da32ae8aa80af3e5066cf2
syedam <syedam@whoseonefind-lm.(none)>
2010-04-27 23:12:09 +0530

    Eights ( Nice solution )

---
+++ EIGHTS/eights.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int cases;
+	cin>>cases;
+	while ( cases-- )
+	{
+		long long num ;
+		cin>>num;
+		long long ans ;
+		ans = 250*num-58;
+		cout<<ans<<endl;
+	}
+}
+
